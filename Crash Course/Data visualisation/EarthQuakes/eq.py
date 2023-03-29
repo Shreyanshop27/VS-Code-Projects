772d30e1c4e3ace2a3748b9e080e7707e08f3d42
Shreynshop27 <sv27102008@gmail.com>
2023-03-29 11:04:04 +0530

    commit

--- Crash Course/Data visualisation/EarthQuakes/eq.py
+++ Crash Course/Data visualisation/EarthQuakes/eq.py
@@ -8,27 +8,34 @@ with open (file) as f:
     data=json.load(f)
 datadic=data['features']
 
-mags,lons,lats=[],[],[]
+mags,lons,lats,hover_text=[],[],[],[]
 for dic in datadic:
     mag=dic['properties']['mag']
     lon=dic['geometry']['coordinates'][0]
     lat=dic['geometry']['coordinates'][1]    
+    title=dic['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_text.append(title)
 
 cordinate = [{
  'type': 'scattergeo',
  'lon': lons,
  'lat': lats,
+ 'text':hover_text,
  'marker': {
  'size': [15*mag for mag in mags],
+ 'color': mags,
+ 'colorscale':'Viridis',
+ 'reversescale':True,
+ 'colorbar':{'title':"Magnitude"}
  },
 }]
 my_layout=Layout(title="Earthquakes in past hour")
 fig={'data':cordinate,'layout':my_layout}
 offline.plot(fig,filename='geoeq.html')
 
- 
+
 
 
